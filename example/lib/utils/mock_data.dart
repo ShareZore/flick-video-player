888c492f61ac16cc406d92f5fdab9ce37eb66d5c
fx <qiangqiang25@126.com>
2022-12-15 15:39:57 +0800

    调试

--- example/lib/utils/mock_data.dart
+++ example/lib/utils/mock_data.dart
@@ -4,7 +4,7 @@ Map<String, dynamic> mockData = {
       "title": "Rio from Above",
       "image": "images/rio_from_above_poster.jpg",
       "trailer_url":
-          "https://github.com/GeekyAnts/flick-video-player-demo-videos/blob/master/example/rio_from_above_compressed.mp4?raw=true",
+          'https://video-file.ddhmit.net/file/url?t=e41057ba02053808168cc20238d042a1a70dd809e5bd5975cee18bd90062d793015e1b7bf6c354f1d30553150135096568ca47f43e1adf616305ced6150629ddd8f58d5be4beadaf2a487330ca26b080d74b91367a1f4f2171251e31fbf0420e75bc42ead7826227de12ca3f8ebefd41a699dc5a3dbf0914db7f65c160152094',
     },
     {
       "title": "The Valley",
